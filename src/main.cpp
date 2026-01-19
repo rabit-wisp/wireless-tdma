cf7e5831583ec856bcca77803192e5229bd45018
Memet Bilgin <memetb@gmail.com>
2026-01-19 22:08:45 +0300

    add a beacon-only mode to allow cli testing and instrumentation

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,17 @@ R"(Cooperative TDMA scheduler
 
 Usage:
   tc-tdma <INTERFACE> <SLOT> [options]
+  tc-tdma <INTERFACE> --show-beacons
 
 Options:
   <INTERFACE>                name of interface to attach to (e.g. wlan0)
   <SLOT>                     ordinal 0-based index of TDMA slot
+  --show-beacons             don't do TDMA, simply show beacon frame statistics
   --slots-per-frame=SLOTS    number of slots per TDMA frame [default: 10]
   --frame-TUs=TUs            Time Units for each TDMA frame [default: 10]
   --buffer-size=SIZE         number of packets to buffer during plug period [default: 10240]
   --count=COUNT              if specified, only run for specified number of TDMA frames
+  --beacon-timeout=TIMEOUT   exit with error if first beacon doesn't arrive within timeout
   --verbose                  show beacons and various things
 
 This program effectively does these 4 actions:
@@ -42,23 +45,51 @@ This program effectively does these 4 actions:
  *   tc qdisc del dev wlan0 root
  */
 
+int show_beacon_stats(const std::string& interface)
+{
+    Beacon beacon(interface, [&](auto ts, auto... args){}, true, true);
+
+    static std::condition_variable terminate;
+    std::mutex mutex;
+
+    auto signal_handler = [](int signal) {
+        if (signal == SIGHUP || signal == SIGINT || signal == SIGTERM) {
+            terminate.notify_all();
+        }
+    };
+
+    std::signal(SIGHUP, signal_handler);
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+
+    std::unique_lock<std::mutex> lock(mutex);
+    terminate.wait(lock);
+    return 0;
+}
+
 int main(int argc, const char* argv[])
 {
     auto args = docopt::docopt(USAGE, {argv + 1, argv + argc});
 
 
+    bool beacon_only = args["--show-beacons"].asBool();
     std::string interface = args["<INTERFACE>"].asString();
-    size_t slotNumber = args["<SLOT>"].asLong();
+    size_t slotNumber = beacon_only ? 0 : args["<SLOT>"].asLong();
     size_t slotsPerFrame = args["--slots-per-frame"].asLong();
     size_t frameTUs = args["--frame-TUs"].asLong();
     auto frameDuration = std::chrono::microseconds(frameTUs * 1024);
 
+    using ms = std::chrono::milliseconds;
+    std::optional<ms> timeout = (args["--beacon-timeout"] ?
+                                 std::optional<ms>(ms(args["--beacon-timeout"].asLong())) : std::nullopt);
     std::optional<size_t> pollCount = args["--count"] ? std::optional<size_t>(args["--count"].asLong()) : std::nullopt;
-
     bool verbose = args["--verbose"].asBool();
 
     try
     {
+        if (beacon_only)
+            return show_beacon_stats(interface);
+
         QdiscController plug(interface, args["--buffer-size"].asLong(), verbose); // qdisc plug controller
 
         std::condition_variable first_beacon;
