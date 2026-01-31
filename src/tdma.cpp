9a8dabdcfb2c4a27898faa97c3db24cbc4657fc9
Memet Bilgin <memetb@gmail.com>
2026-01-31 19:34:09 +0300

    log output cleanup

--- src/tdma.cpp
+++ src/tdma.cpp
@@ -62,7 +62,7 @@ void TDMAScheduler::resynchronize(TDMAScheduler::timestamp beacon, size_t TUs)
     // TODO: think about the synchronization here
     //std::lock_guard<std::mutex> guard(mutex);
     if (verbose)
-        std::cout << "qdisc SYNC: @" << std::fixed << std::setprecision(6) << (double)(beacon.time_since_epoch().count()) / 1e9
+        std::cout << "qdisc: SYNC @" << std::fixed << std::setprecision(6) << (double)(beacon.time_since_epoch().count()) / 1e9
                   << " s   |   beacon interval: " << TUs << " TUs" << std::endl;
 
     frame_start = beacon + frame_duration;
